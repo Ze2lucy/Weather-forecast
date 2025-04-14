4902c12dc123be7e2f01de2bdb3a84bab1b6851f
Ze2-lucy <zethulucysitole@gmail.com>
2025-04-14 14:30:48 +0200

    Added an API key

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+function searchCity(city) {
+  let apiKey = "d99e9a025t549f2boab03f4d6b088862";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
+  console.log("API URL:", apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#weather-app-city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
